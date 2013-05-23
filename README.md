ddfdf0a4b829e9e6e7af75ed73373a375bb0fc92
Eric Brelsford <ebrelsford@gmail.com>
2013-05-23 00:21:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+Leaflet.loading
+===============
+
+Leaflet.loading is a simple loading control for [Leaflet][]. An unobtrusive
+loading indicator is added below the zoom control if one exists. The indicator
+is visible when tiles are loading or when other data is loading, as indicated by
+firing custom events on a map.
+
+
+## Usage
+
+Include `Control.Loading.js` and create a map with `loadingControl: true` in its
+options. Then style your loading indicator. `Control.Loading.css` contains a 
+start in this direction. The simplest case would be adding a 16 x 16 loading gif
+in `.leaflet-control-loading`.
+
+Once the above is complete you will have a loading indicator that only appears
+when tiles are loading. 
+
+If you want to show the loading indicator while other AJAX requests or something
+else is occurring, simply fire the `dataloading` event on your map when you 
+begin loading and `dataload` when you are finished loading. The control tracks 
+the number of concurrent loaders, so it is your responsibility to ensure that 
+the `dataloading` and `dataload` are called symmetrically.
+
+ [Leaflet]: https://github.com/Leaflet/Leaflet
---
+++ src/Control.Loading.js
@@ -0,0 +1,112 @@
+/*
+ * L.Control.Loading is a control that shows a loading indicator when tiles are
+ * loading or when map-related AJAX requests are taking place.
+ */
+L.Control.Loading = L.Control.extend({
+    options: {
+        position: 'topleft',
+    },
+
+    initialize: function(options) {
+        L.setOptions(this, options);
+        this._dataLoaders = 0;
+    },
+
+    onAdd: function(map) {
+        // Create the loading indicator
+        var classes = 'leaflet-control-loading leaflet-bar-part last';
+        var container;
+        if (map.zoomControl) {
+            // If there is a zoom control, hook into the bottom of it
+            container = map.zoomControl._container;
+            classes += ' leaflet-bar-part-bottom';
+        }
+        else {
+            // Otherwise, create a container for it
+            container = L.DomUtil.create('div', 'leaflet-control-zoom');
+        }
+        this._indicator = L.DomUtil.create('a', classes, container);
+        return container;
+    },
+
+    addLoader: function() {
+        // Add to the loaders we are tracking
+        this._dataLoaders++;
+
+        // If there are some loaders, show the indicator
+        if (this._dataLoaders > 0) {
+            this._showIndicator();
+        }
+    },
+
+    removeLoader: function() {
+        // Subtract from the loaders we are tracking
+        this._dataLoaders--;
+
+        // If there are no loaders left, remove the indicator
+        if (this._dataLoaders <= 0) {
+            // If there is a mismatch don't let the number of loaders
+            // become negative
+            this._dataLoaders = 0;
+            this._hideIndicator();
+        }
+    },
+
+    _showIndicator: function() {
+        // Show loading indicator
+        L.DomUtil.addClass(this._indicator, 'is-loading');
+
+        // If map.zoomControl exists, make the zoom-out button not last
+        if (this._map.zoomControl) {
+            L.DomUtil.removeClass(this._map.zoomControl._zoomOutButton, 'leaflet-bar-part-bottom');
+        }
+    },
+
+    _hideIndicator: function() {
+        // Hide loading indicator
+        L.DomUtil.removeClass(this._indicator, 'is-loading');
+
+        // If map.zoomControl exists, make the zoom-out button last
+        if (this._map.zoomControl) {
+            L.DomUtil.addClass(this._map.zoomControl._zoomOutButton, 'leaflet-bar-part-bottom');
+        }
+    },
+
+});
+
+L.Map.addInitHook(function () {
+    if (!this.options.loadingControl) return;
+
+    // Create and add the control to the map
+    this.loadingControl = L.Control.loading();
+    this.addControl(this.loadingControl);
+
+    // When a layer is added to the map, add listeners for begin and end
+    // of load
+    this.on('layeradd', function(e) {
+        e.layer.on({
+            loading: function(e) {
+                this._map.loadingControl.addLoader();
+            },
+            load: function(e) {
+                this._map.loadingControl.removeLoader();
+            },
+        });
+    });
+
+    // Add listeners to the map for (custom) dataloading and dataload
+    // events, eg, for AJAX calls that affect the map but will not be
+    // reflected in the above layer events.
+    this.on({
+        dataloading: function(e) {
+            this.loadingControl.addLoader();
+        },
+        dataload: function(e) {
+            this.loadingControl.removeLoader();
+        },
+    });
+});
+
+L.Control.loading = function(options) {
+    return new L.Control.Loading(options);
+};
